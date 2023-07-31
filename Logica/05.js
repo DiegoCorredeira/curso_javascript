63455e424b42f95935671e363942ed7e34306858
Diego <diegosccpinto@gmail.com>
2023-07-31 13:36:21 -0300

    add modelo html para o futuro

---
+++ Logica/05.js
@@ -0,0 +1,7 @@
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+    console.log("O número está entre 0 e 5");
+} else if (numero >= 6 && numero <= 10) {
+    console.log("O número está entre 6 e 10");
+} 
