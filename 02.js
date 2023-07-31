d5f6b56497b23b144430de997ed69e7641b6b7fb
Diego <diegosccpinto@gmail.com>
2023-07-31 11:54:24 -0300

    aula 1 e 2

---
+++ 02.js
@@ -0,0 +1,8 @@
+/*
+Operadores Lógicos
+
+&& -> AND -> E -> Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU -> Pelo menos uma expressão precisa ser verdadeira para retornar true
+! -> NOT -> NÃO -> Nega uma expressão. Se verdadeira, retorna false. Se falsa, retorna true
+*/
+
--- Logica/01.js
+++ Logica/01.js
@@ -13,3 +13,33 @@ Operadores de comparação
 
 const comp = 10 > 5;
 console.log(comp); // true
+
+const comp2 = 10 < 5;
+console.log(comp2); // false
+
+const comp3 = 10 >= 5;
+console.log(comp3); // true
+
+const comp4 = 10 <= 5;
+console.log(comp4); // false
+
+const comp5 = 10 == 5;
+console.log(comp5); // false
+
+const comp6 = 10 == 10;
+console.log(comp6); // true
+
+const comp7 = 10 === 10;
+console.log(comp7); // true
+
+const comp8 = 10 === '10';
+console.log(comp8); // false
+
+const comp9 = 10 != 5;
+console.log(comp9); // true
+
+const comp10 = 10!= 10;
+console.log(comp10); // false
+
+const comp11 = 10!== 10;
+console.log(comp11); // false
