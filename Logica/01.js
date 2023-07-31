489498d1b2feb1ce4ee1ea64bfa0bbde41ee503b
Diego <diegosccpinto@gmail.com>
2023-07-31 11:29:38 -0300

    first commit

---
+++ Logica/01.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+
+> maior que 
+< menor que 
+>= maior ou igual a
+<= menor ou igual a
+== igualdade (valor) *****
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) *****
+!== diferente estrito (valor e tipo)
+*/
+
+const comp = 10 > 5;
+console.log(comp); // true
