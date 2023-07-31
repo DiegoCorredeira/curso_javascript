2c2992387298a45beaf6228adb7dd235698f8ad7
Diego <diegosccpinto@gmail.com>
2023-07-31 13:18:42 -0300

    aula 4

---
+++ 04.js
@@ -0,0 +1,17 @@
+
+// If pode ser usado sozinho
+// Sempre que utilizo a palavra else, preciso de um if antes
+// Eu posso ter vários else ifs na checagem
+// Só posso ter um else na checagem
+// Podemos usar condições sem else if, utilizando apenas if e else
+
+
+const hora = 12;
+
+if (hora >= 0 && hora <= 11) {
+    console.log("Bom dia");
+} else if (hora >= 12 && hora <= 17) {
+    console.log("Boa tarde");
+} else{
+    console.log("Boa noite");
+}
