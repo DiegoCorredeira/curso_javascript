ee17566ca78623c06ad81fec71c8c2333f0afe69
Diego <diegosccpinto@gmail.com>
2023-08-01 11:01:36 -0300

    exercicio de formatacao de datas

--- Modulo 1 - Logica/07.js
+++ Modulo 1 - Logica/07.js
@@ -3,5 +3,18 @@
  * 
  */
 
-const data = new Date(2023, 7, 1);
-console.log(data.toString());
+// const data = new Date(2023, 7, 1); // a, m, d , h, M, s , ms
+// console.log(data.toString());
+
+
+const data = new Date() // formato mais utilizado
+const dia = data.getDate(); 
+const mes = data.getMonth()
+const ano = data.getFullYear()
+const hora = data.getHours()
+const minutos = data.getMinutes()
+const segundos = data.getSeconds()
+const milis = data.getMilliseconds()
+const diaSemana = data.getDay()
+
+console.log(dia, mes, ano, hora, minutos, segundos, milis, diaSemana);
