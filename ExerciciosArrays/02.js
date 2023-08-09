b57f84efc0fba2af50add285143b96e44c16a127
Diego <diegosccpinto@gmail.com>
2023-08-08 22:03:16 -0300

    Exercicios de arrays

---
+++ ExerciciosArrays/02.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * Adicionar e remover elementos:
+ * Crie um array vazio.
+ * Adicione três números inteiros a este array.
+ * Remova o último elemento do array.
+ * Adicione um novo número no final.
+ * Imprima o array resultante no console.
+ */
+
+// Criando um array vazio
+const array = [];
+// Adiciona 3 elementos
+array.push(2, 3, 4);
+// Remove o ultimo elemento
+array.pop();
+// Adiciona um elemento
+array.push(5);
+
+console.log(array);
---
+++ ExerciciosArrays/06.js
@@ -0,0 +1,22 @@
+/**
+ * Ordenar um array:
+ * Crie um array de palavras.
+ * Ordene o array em ordem alfabética crescente.
+ * Ordene o array em ordem alfabética decrescente.
+ * Imprima o array em ambas as ordenações no console.
+ */
+
+// Criando um array de palavras
+const palavras = ["casa", "carro", "bicicleta", "moto", "aviao"];
+
+// Ordenando o array em ordem alfabetica crescente
+palavras.sort();
+
+// Imprimindo o array
+console.log(palavras);
+
+// Ordenando o array em ordem alfabetica decrescente
+palavras.reverse();
+
+// Imprimindo o array
+console.log(palavras);
